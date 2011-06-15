e6b865544ef8c7916bf1baa2ea20d92f9da27eb0
Sergey Seleznev <18219010+sergey-seleznev@users.noreply.github.com>
2011-06-15 19:08:41 +0000

    add: public subfolders are now monitored for files as well add: automatic public sync folder detection fix: non-Latin and special characters now encoded correctly in URLs fix: slot-based popups, that use first free slot and will never be overlapped (you can test it while previewing timeout) add: public sync folder setting is hidden, when it is auto-detected add: timeout interval is displayed in popup previews fix: a lot of major and minor code refactoring

---
+++ DropBoxLinker/DropBoxLinker/Tray.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows;
+using DropBoxLinker.Properties;
+using WinForms = System.Windows.Forms;
+
+namespace DropBoxLinker
+{
+    public partial class App : Application
+    {
+        private static WinForms.NotifyIcon tray;
+
+        // launch / destroy
+        public static void LaunchTrayIcon()
+        {
+            // load icon
+            var uri = new Uri(@"/graphics/tray.ico", UriKind.Relative);
+            var resource = Application.GetResourceStream(uri);
+
+            // create menu items
+            var menu = new WinForms.MenuItem[] {
+                new WinForms.MenuItem("Open Public", OnTrayOpenFolder) { DefaultItem = true },
+                new WinForms.MenuItem("Settings", OnTraySettings),
+                new WinForms.MenuItem("About", OnTrayAbout),
+                new WinForms.MenuItem("-"),
+                new WinForms.MenuItem("Exit", OnTrayExit) };
+
+            // create tray icon
+            tray = new WinForms.NotifyIcon {
+                Text = "DropBox Linker",
+                Icon = new Icon(resource.Stream),
+                ContextMenu = new WinForms.ContextMenu(menu),
+                Visible = true };
+            tray.DoubleClick += OnTrayOpenFolder;
+        }
+        public static void DestroyTrayIcon()
+        {
+            if (tray != null)
+            {
+                tray.Visible = false;
+                tray.Dispose();
+                tray = null;
+            }
+        }
+
+        // menu item click handlers
+        private static void OnTrayOpenFolder(object sender, EventArgs e)
+        {
+            Process.Start(Settings.Default.SyncDirectory);
+        }
+        private static void OnTraySettings(object sender, EventArgs e)
+        {
+            new frmSettings().ShowDialog();
+        }
+        private static void OnTrayAbout(object sender, EventArgs e)
+        {
+            new frmAbout().ShowDialog();
+        }
+        private static void OnTrayExit(object sender, EventArgs e)
+        {
+            StopWatcher();
+            DestroyTrayIcon();
+            Application.Current.Shutdown();
+        }
+
+    }
+}
--- DropBoxLinker/DropBoxLinker/frmNotify.xaml.cs
+++ DropBoxLinker/DropBoxLinker/frmNotify.xaml.cs
@@ -1,78 +1,129 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using DropBoxLinker.Properties;
 
 namespace DropBoxLinker
 {
-
-
     public partial class frmNotify : Window
     {
+        // launchers
+        public static void Launch(string file, NotifyType type)
+        {
+            // create window
+            var n = new frmNotify();
+
+            // take a slot
+            n.AssignSlot();
+
+            // set file info
+            var info = new FileInfo(file);
+            n.txtTitle.Text = info.Name.Replace(info.Extension, "");
+            n.txtExt.Text = info.Extension.ToLower();
+
+            // set state
+            n.txtState.Text = NotifyTypeText(type);
+
+            // set timeout
+            n.PopupHideAnimation.BeginTime =
+                TimeSpan.FromSeconds(
+                Settings.Default.PopupTimeout);
+
+            // set opacity
+            n.Opacity = 0.0d;
+
+            // set location on loaded
+            n.Loaded += n.ArrangeWindow;
+
+            // start-up
+            n.Show();
+        }
+        public static void Preview(float timeout)
+        {
+            // create window
+            var n = new frmNotify();
+
+            // take a slot
+            n.AssignSlot();
+
+            // set file info
+            n.txtTitle.Text = "popup preview";
+            n.txtExt.Text = String.Format("  {0:F1} sec.", timeout);
+
+            // set state
+            n.txtState.Text = NotifyTypeText(NotifyType.Preview);
+
+            // set timeout
+            n.PopupHideAnimation.BeginTime =
+                TimeSpan.FromSeconds(timeout);
+
+            // set opacity
+            n.Opacity = 0.0d;
+
+            // set location on loaded
+            n.Loaded += n.ArrangeWindow;
+
+            // start-up
+            n.Show();
+        }     
+
+        // static stuff
+        private static List<int> BusySlots = new List<int>();
+        private static string NotifyTypeText(NotifyType type)
+        {
+            switch (type)
+            {
+                case NotifyType.Preview: return "you can preview popup timeout";
+
+                case NotifyType.Set: return "public link copied to clipboard";
+                case NotifyType.Append: return "public link appended to clipboard";
+                case NotifyType.Update: return "public link updated in clipboard";
+                case NotifyType.Remove: return "public link removed from clipboard";
+
+                default: return "";
+            }
+        }    
+
+        // instance stuff
         public frmNotify()
         {
             InitializeComponent();
         }
-
-        private String NotifyTypeText(NotifyType type)
+        private int CurrentSlot = 0;
+        private void AssignSlot()
         {
-            switch (type)
+            lock (BusySlots)
             {
-                case NotifyType.Preview:    return "that's just a popup preview";
+                // find first free slot
+                while (BusySlots.Contains(CurrentSlot)) {
+                    CurrentSlot++; }
 
-                case NotifyType.Set:        return "public link copied to clipboard";
-                case NotifyType.Append:     return "public link appended to clipboard";
-                case NotifyType.Update:     return "public link updated in clipboard";
-                case NotifyType.Remove:     return "public link removed from clipboard";
-
-                default: return "";
+                // mark it busy
+                BusySlots.Add(CurrentSlot);
             }
         }
-
-        public void Launch(int index, string title, string ext, NotifyType type, float? timeout = null)
+        private void ArrangeWindow(object sender, EventArgs e)
         {
-            // set file info
-            if (type != NotifyType.Preview) {
-                txtTitle.Text = title;
-                txtExt.Text = ext; }
-            else {
-                txtTitle.Text = "some-sample-file";
-                txtExt.Text = ".txt"; }
-            
-            // set state
-            txtState.Text = NotifyTypeText(type);
-                        
-            // set timeout
-            PopupHideAnimation.BeginTime =
-                TimeSpan.FromSeconds((timeout.HasValue) ?
-                timeout.Value : Settings.Default.PopupTimeout);
+            // screen resolution (w/o menu)
+            var screen_width = SystemParameters.WorkArea.Width;
+            var screen_height = SystemParameters.WorkArea.Height;
 
-            // set opacity
-            Opacity = 0.0d;
-
-            // change location on loaded
-            Loaded += (o, e) =>
-            {
-                // screen resolution (w/o menu)
-                var screen_width = SystemParameters.WorkArea.Width;
-                var screen_height = SystemParameters.WorkArea.Height;
-
-                // set position
-                Top = screen_height - ActualHeight - 5 - (index - 1) * (ActualHeight + 10);
-                Left = screen_width - ActualWidth - 5;
-            };
-            
-            // start-up
-            Show();
+            // set position
+            Top = screen_height - ActualHeight - 5 - CurrentSlot * (ActualHeight + 10);
+            Left = screen_width - ActualWidth - 5;
         }
-
         private void AnimationComplete(object sender, EventArgs e)
         {
-            // finished
+            // free slot
+            lock (BusySlots) {
+                BusySlots.Remove(CurrentSlot); }
+
             Close();
-        }
-        
+        }  
     }
 
+    // notification types
     public enum NotifyType
     {
         Preview,    // preview the popup
